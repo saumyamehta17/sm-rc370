9ef2c47b987f7cc6b3c3ed8becc54764a566e1b8
sweety <smehta1289@gmail.com>
2013-07-10 21:50:14 +0530

    advance search and sort

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SmRc370::Application.config.secret_token = '80360b3d13b4b955629745829cd065950b36de4758808c2935c42e79af9d8a0a6e60347f94fb9d11ea60f73b575c6d731f6b0a8149497ff565a3ae11d78d20d4'
